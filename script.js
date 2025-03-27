2f29b18293fd628244d78d19e9cfec3afd8ec067
DROPTASK <vanshadd002@gmail.com>
2025-03-27 09:37:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let projects = [
-        "Bless", "Dawn", "Grass", "Graident", "One Football",
-        "Teneo", "Nexus", "Nodepay", "Blockmesh", "Flow3",
-        "Mygate", "Treasury", "Layeredge", "Common", "Beamable",
-        "Giza", "Exhabits", "Sogni", "Solflare NFT", "Deshare [Cess]",
-        "Wonix", "Arch", "Dvin", "Blockscout", "Malda", "Somnia",
-        "Social Incentive", "Billions", "Pod [Dreamers]"
-    ];
-
+    let projects = [...]; // Add your projects here
     let completedProjects = JSON.parse(localStorage.getItem("removed")) || [];
     let earnings = JSON.parse(localStorage.getItem("earnings")) || [];
 
